a9d63511e1a757d73b4569da435881c73f41e1d3
aliz <aliz@xd.lan>
2017-11-16 16:11:07 +0800

    sanitise path better

--- trunk/uploadsymbols.bat
+++ trunk/uploadsymbols.bat
@@ -1,3 +1,3 @@
 cd %1
-%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -File uploadsymbols.ps1 %2
-
+arg=%%~dpnx2
+%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -File uploadsymbols.ps1 %arg
