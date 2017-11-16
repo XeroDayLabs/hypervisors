47bca640397556128ad2fd7556f5de92542cf140
aliz <aliz@xd.lan>
2017-11-16 16:26:10 +0800

    Make sure trailing slashes on path are removed before uploading symbols via powershell

--- trunk/uploadsymbols.bat
+++ trunk/uploadsymbols.bat
@@ -1,3 +1,6 @@
 cd %1
-arg=%%~dpnx2
-%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -File uploadsymbols.ps1 %arg%
+set arg=%~dpnx2
+REM Remove any trailing backslash
+IF %arg:~-1%==\ set arg=%arg:~0,-1%
+%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -File uploadsymbols.ps1 "%arg%"
+
