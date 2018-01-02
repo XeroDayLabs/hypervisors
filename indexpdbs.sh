969f19b3f50e06923e953ba970d5e73c9ddecdd0
aliz <aliz@xd.lan>
2018-01-02 14:41:12 +0800

    Add scripts to perform source indexing

---
+++ indexpdbs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+baseURL=http://files.xd.lan:7990/projects/XD/repos/hypervisors/browse
+
+cat tmp-pre.ini > tmp.ini
+
+gitfiles=`git ls-files -s`
+commitid=`git rev-parse HEAD`
+echo "$gitfiles" | while read gitfile; do
+  hash=`echo $gitfile | cut -b 8-47`
+  filename=`echo $gitfile | cut -b 51-999`
+  origsrc=`cygpath -w $(pwd)`/${filename}
+  tempfile=$hash/${filename}
+  url=${baseURL}/${filename}\&at=${commitid}?raw
+  echo $origsrc*$tempfile*$url >> tmp.ini
+done 
+
+echo SRCSRV: end >> tmp.ini
+
+echo "`find -name \"*.pdb\"`" | while read file; do
+"/cygdrive/c/Program Files (x86)/Windows Kits/10/Debuggers/x64/srcsrv/pdbstr.exe" -w -p:"$file" -s:srcsrv -i:tmp.ini
+
+done
