3f268a6e96af876a44fe8fa26ed40745fa390f15
aliz <aliz@xd.lan>
2018-01-02 16:49:51 +0800

    Index source via powershell instead of via cygwin; integrate it with symbol upload script

--- trunk/uploadsymbols.ps1
+++ trunk/uploadsymbols.ps1
@@ -1,6 +1,43 @@
 $srcDir = $args[0]
+
+# Do some source indexing.
+$baseURL="http://files.xd.lan:7990/projects/XD/repos/hypervisors/browse"
+$commitid=(git rev-parse HEAD)
+Copy-Item tmp-pre.ini tmp.ini
+
+$tmpfilename=$env:TEMP + '\tmp.ini'
+$tmpIni = [System.io.File]::Open($tmpfilename, 'Append', 'Write', 'None')
+
+# First off, do a 'git ls-files' to get hashes:
+$lines=(git ls-files -s).split("`n");
+$enc=[system.Text.Encoding]::UTF8
+ForEach($line in  $lines)
+{
+  $hash = $line.Substring(7,40);
+  $filename = $line.Substring(50);
+  $filename = $filename.Replace('/', '\');
+  $tempfile=$hash + '\' + $filename
+  $url=$baseURL + '\' + $filename + "?raw&at=" + $commitid
+  $lineOut = $filename + "*" + $tempfile + "*" + $url
+  $lineOutBytes = $enc.GetBytes($lineOut)
+  $tmpIni.write($lineOutBytes, 0, $lineOutBytes.Length)
+} 
+
+$lineOutBytes = $enc.GetBytes("SRCSRV: end")
+$tmpIni.write($lineOutBytes, 0, $lineOutBytes.length)
+
+$tmpIni.Close()
+
+# Run the SDK tool to inject path info
+ForEach($pdbfile in Get-ChildItem $srcDir\*.pdb)
+{
+  "c:\Program Files (x86)\Windows Kits\10\Debuggers\x64\srcsrv\pdbstr.exe -w -p:`"$pdbfile`" -s:srcsrv -i:$tmpfilename"
+}
+
+# And copy PDBs up.
 $rand = Get-Random
-$tmpDir = "\\applejack\incomingSymbols\" + $env.computername + $rand + $SrcDir.Replace("\", "_").Replace(":", "_").Replace(" ", "_")
-echo $tmpDir
+$tmpDir = "\\applejack\incomingSymbols\" + $env:computername + $rand + $SrcDir.Replace("\", "_").Replace(":", "_").Replace(" ", "_")
+
 New-Item $tmpDir -type Directory
+echo $srcDir
 Copy-Item $srcDir\*.pdb $tmpDir
