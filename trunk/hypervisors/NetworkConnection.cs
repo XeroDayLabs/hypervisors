19f9cc85ac2f237b64e7f88155ebad01ae6272eb
aliz <aliz@xd.lan>
2018-05-09 14:47:22 +0800

    Be more careful about when we call WNetCancelConnection2; especially, swallow any exceptions if it is called in the object finalizer

--- trunk/hypervisors/NetworkConnection.cs
+++ trunk/hypervisors/NetworkConnection.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.ComponentModel;
 using System.Net;
 using System.Runtime.InteropServices;
@@ -6,17 +7,21 @@ using System.Runtime.InteropServices;
 namespace hypervisors
 {
     /// <summary>
-    /// ty based stackoverflow
+    /// This came from stackoverflow, with some bugfixes added
     /// </summary>
     public class NetworkConnection : IDisposable
     {
-        readonly string _networkName;
+        private string _networkName;
 
-        public NetworkConnection(string networkName,
-            NetworkCredential credentials, out Exception e)
+        private static ConcurrentDictionary<string, bool> openConnections = new ConcurrentDictionary<string, bool>();
+
+        public NetworkConnection(string networkName, NetworkCredential credentials, out Exception e)
         {
             e = null;
             _networkName = networkName;
+
+            if (!openConnections.TryAdd(_networkName, true))
+                throw new Exception("Duplicate NetworkConnection classes for network name '" + _networkName + "'");
 
             NetResource netResource = new NetResource()
             {
@@ -42,23 +47,48 @@ namespace hypervisors
             if (result != 0)
             {
                 e = new Win32Exception(result, "Error connecting to remote share, GLE " + Marshal.GetLastWin32Error());
+                bool foo;
+                if (!openConnections.TryRemove(_networkName, out foo))
+                {
+                    string tmp = _networkName;
+                    _networkName = null;
+                    throw new Exception("Couldn't remove network credential '" + tmp + "'");
+                }
+                _networkName = null;
             }
         }
 
         ~NetworkConnection()
         {
-            Dispose(false);
+            try
+            {
+                destroyConnection();
+            }
+            catch (Exception)
+            {
+                // :(
+            }
         }
 
         public void Dispose()
         {
-            Dispose(true);
+            destroyConnection();
             GC.SuppressFinalize(this);
         }
 
-        protected virtual void Dispose(bool disposing)
+        protected virtual void destroyConnection()
         {
-            WNetCancelConnection2(_networkName, 0, true);
+            if (_networkName != null)
+            {
+                WNetCancelConnection2(_networkName, 0, true);
+                bool foo;
+                if (!openConnections.TryRemove(_networkName, out foo))
+                {
+                    // Oh no! We can't safely throw from the finalizer thread, how can we notify the user?!
+                    openConnections = null; // >:)
+                }
+                _networkName = null;
+            }
         }
 
         [DllImport("mpr.dll", SetLastError = true)]
